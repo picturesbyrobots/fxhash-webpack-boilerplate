7d4be51f0a99c49b4f4fc6a8c1cd3a3e0c997640
Dave Tennent <picturesbyrobots@gmail.com>
2021-11-18 10:13:58 -0500

    better minimal project

--- public/fxhash-p5-boilerplate.js
+++ public/fxhash-p5-boilerplate.js
@@ -1,89 +1,36 @@
 
   
-const init = () => {
 
 
-}
-var numMovers = 200;
-const randRange = (min,max) => {
-    return fxrand() * (max - min) + min;
-}
 
-class Mover {
-    constructor(xpos, ypos, speed, size, amp, per)
-    {
-        this.xpos = xpos;
-        this.ypos = ypos
-        this.speed = speed
-        this.size = size
-        this.amp = amp
-        this.per = per
-        this.text = ''
-        let emoji = '❤✨-~'
-        let emoji_chance = randRange(0,100)
-        if(emoji_chance < 40)
-        {
-            this.text = emoji.charAt(randRange(0,emoji.length))
-        }
-       
-    }
-    update()
-    {
-        this.xpos -= this.speed * (deltaTime / 30);
-        let y_off = (this.amp * .01) * Math.sin(TWO_PI * frameCount / this.per)
-        this.ypos += y_off
-        
-
-        if(this.xpos <= -100)
-        {
-            this.xpos = randRange(window.innerWidth,window.innerWidth + 1500)
-            this.ypos = randRange(0,800)
-        }
-    }
-    draw ()
-    {
-        if(this.text === '') 
-        {
-            ellipse(this.xpos, this.ypos, this.size,this.size);
-        }
-        else {
-            textSize(this.size * 3)
-            textAlign(CENTER)
-            text(this.text, this.xpos, this.ypos)
-        }
-    }
-}
-
-let movers = []
 
 function setup()
 {
-    createCanvas(window.innerWidth - 10, window.innerHeight -10)
-    for(var i = 0; i < numMovers; i++)
-    {
-        var mover = new Mover(
-                randRange(window.innerWidth,window.innerWidth + 1400), 
-                randRange(0, 800),
-                randRange(1,5),
-                randRange(4,10),
-                randRange(10, 30),
-                randRange(120, 3000));
-        movers.push(mover);
+    const randRange = (min,max) => {
+        return fxrand() * (max - min) + min;
     }
+    let colors = 
+    [
+        [255,0,0],
+        [0,255,0],
+        [0,0,255]
+    ]
+    createCanvas(window.innerWidth - 10, window.innerHeight -10)
+    
+
+    let c_data= colors[Math.floor(randRange(0,colors.length))]
+    fill(c_data[0], c_data[1], c_data[2])
+    
 
 }
 
+
+
 function draw()
 {
-    background(0);
-    color(255)
-    noFill()
+    background(100,100,100);
+    
     stroke(255,255,255)
-
-    for(var i = 0; i < numMovers; i++)
-    {
-        var this_mover = movers[i];
-        this_mover.update();
-        this_mover.draw();
-    }
+    ellipse(window.innerWidth * .5, window.innerHeight * .5, (Math.sin(millis() * .00100)  * 400))
+    
 }
